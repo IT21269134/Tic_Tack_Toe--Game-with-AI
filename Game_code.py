4a23d25df144499856a92993f2f727f859bc5afe
Harini chamathka <harinichamathka14@gmail.com>
2023-07-18 02:25:28 +0530

    Update code files

--- Game_code.py
+++ Game_code.py
@@ -40,5 +40,29 @@ def constBoard(board):
       print("O ", end =" ");
   print("\n\n");
 
+# create user1Turn function
+def user1Turn(board):
+  pos =input("Enter X's position from [1,2,3,4,...9]: ");
+  pos =int(pos);
+  if(board[pos-1]!=0):
+    print("Wrong move");
+    exit(0);
+  board[pos-1]=-1;
 
+# Create user2Turn function
+def user2Turn(board):
+  pos =input("Enter O's position from [1,2,3,4,...9]: ");
+  pos =int(pos);
+  if(board[pos-1]!=0):
+    print("Wrong move");
+    exit(0);
+  board[pos-1]=1;
+
+# Create compTurn function
+def analyseBoard(board):
+  cb =[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+  for i in range(0,8):
+    if(board[cb[i][0]]!=0 and board[cb[i][0]]==board[cb[i][1]] and board[cb[i][1]]==board[cb[i][2]] ):
+      return board[cb[i][0]];
+    return 0;
 
